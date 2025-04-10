e75c218145f4c2d5ab52719bdbe4c47eedb149d8
hacker del barco <61146153+maridasi47500@users.noreply.github.com>
2025-04-10 14:27:17 -0300

    hey

---
+++ lancer_groups.rb
@@ -0,0 +1,81 @@
+require 'watir'
+require 'addressable/uri'
+require 'i18n'
+require 'json'
+require 'date'
+require 'logger'
+require 'test/unit'
+require 'watir-jquery'
+def okurl(ok)
+    return Addressable::URI.escape(ok)
+end
+
+d = DateTime.now
+Watir.default_timeout = 30 
+
+
+                          #=> #<DateTime: 2007-11-19T08:37:48-0600 ...>
+mytime=d.strftime("%m%d%Y%I%M%p")
+logger = Logger.new('logs/app'+mytime+'.log', 3, 10 * 1024 * 1024)
+
+
+I18n.load_path += Dir[File.expand_path("locales") + "/*.yml"]
+I18n.default_locale = :fr # (note that `en` is already the default!)
+wow=Date.today
+
+
+
+links = ["https://www.facebook.com"]
+links.to_a.each do |link|
+      begin
+          if link.include?("facebook")
+              browser = Watir::Browser.new :firefox
+              browser.goto(link)
+              browser.wait_until {|x| x.title != "Facebook - log in or sign up" }
+
+              logger.info("\nhey !!! \nje suis sur un compte anonyme \n ")
+              posts=JSON.parse(File.read('your_recently_followed_history.json'))
+              posts["label_values"][0]["vec"].each do |post|
+                groupname=post["dict"][0]["value"]
+                p post["dict"][0]["label"]
+
+                begin
+                  browser.goto("https://facebook.com/search/top?q="+groupname)
+                  browser.wait_until do |b|
+                      b.text.include?("Suivi(e)") or b.text.include?("n'est pas disponible") or b.text.include?("isn't available right now")
+                  end
+                  next if browser.text.include?("n'est pas disponible") or browser.text.include?("isn't available right now")
+                  browser.execute_script("document.querySelector(\"[aria-label='Suivi(e)']\").click()")
+                  p "Toutes les photo"
+                  browser.wait_until do |b|
+                      b.text.include?("Toutes les photos")
+                  end
+                  browser.execute_script("document.querySelector(\"[aria-label*='Paramètres du profil voir plus d']\").click()")
+                  browser.wait_until do |b|
+                      b.text.include?("Suivi(e)")
+                  end
+                  browser.execute_script("document.querySelector(\"[role=menuitem]:last-child\").click()")
+                  browser.wait_until do |b|
+                      b.text.include?("Paramètres de suivi")
+                  end
+                  browser.execute_script("document.querySelector(\"[role=radio][aria-checked=false]\").outerHTML=''")
+                  browser.execute_script("document.querySelector(\"[role=radio][aria-checked=false]\").click()")
+                  browser.execute_script("document.querySelector(\"[aria-label='Mettre à jour']\").click()")
+                     
+
+                rescue => e
+                  logger.info("\ngroup_id\nerreur \n "+e.message)
+                  logger.error("\nil y a eu un probleme pour le group facebook  numero  "+groupname+" : "+e.message)
+                  next
+    
+                end
+              end
+          end
+      rescue => e
+        logger.error("\nil y a eu un probleme pour la page de  "+link+" : "+e.message)
+        next
+    
+      end
+    
+    
+end
---
+++ lancer_job.rb
@@ -0,0 +1,80 @@
+require 'watir'
+require 'addressable/uri'
+require 'i18n'
+require 'json'
+require 'date'
+require 'logger'
+require 'test/unit'
+require 'watir-jquery'
+def okurl(ok)
+    return Addressable::URI.escape(ok)
+end
+
+d = DateTime.now
+Watir.default_timeout = 30 
+
+
+                          #=> #<DateTime: 2007-11-19T08:37:48-0600 ...>
+mytime=d.strftime("%m%d%Y%I%M%p")
+logger = Logger.new('logs/app'+mytime+'.log', 3, 10 * 1024 * 1024)
+
+
+I18n.load_path += Dir[File.expand_path("locales") + "/*.yml"]
+I18n.default_locale = :fr # (note that `en` is already the default!)
+wow=Date.today
+
+
+
+links = ["https://www.facebook.com"]
+links.to_a.each do |link|
+      begin
+          if link.include?("facebook")
+              browser = Watir::Browser.new :firefox
+              browser.goto(link)
+              browser.wait_until {|x| x.title != "Facebook - log in or sign up" }
+
+              #browser.execute_script("alert('hey anonyme');return false") # Open the rest in new tabs
+              logger.info("\nhey !!! \nje suis sur un compte anonyme \n ")
+              posts=JSON.parse(File.read('posts.json'))
+              posts["data"].each do |post|
+                begin
+                  browser.goto("https://facebook.com/"+post["id"])
+                  browser.wait_until do |b|
+                      debutmsg= post["message"].encode("utf-8")[0..20]
+                      p "LE MESSAGE EST AFFICHE OU PAS:"+b.text.include?(debutmsg).to_s
+                      b.text.include?("Ce contenu n’est pas disponible pour le moment") or b.text.include?("This content isn't available right now") or b.text.include?(debutmsg)  or b.text.include?("Modifier")
+                  end
+                  next if browser.text.include?("contenu n'est pas disponible")
+
+                  next if browser.text.include?("isn't available right now")
+                     
+                  browser.execute_script("document.querySelector(\"[aria-label='Actions pour cette publication'][aria-expanded='false']\").click()")
+                  browser.execute_script("document.querySelector(\"[aria-label='Actions pour cette publication'][aria-expanded='false']\").click()")
+                  logger.info("\nclick actions \n ")
+
+                  browser.wait_until do |b|
+                      b.text.include?("Supprimer la")
+                  end
+                  next if browser.text.include?("Supprimer la photo")
+                  browser.execute_script("document.querySelector(\"[role='menuitem']:nth-child(5)\").click()")
+                  logger.info("\ndemander supprime \n ")
+                  browser.wait_until{|b|b.text.include?("Supprimer")}
+                  browser.execute_script("document.querySelector('[aria-label=Supprimer]').click()")
+                  logger.info("\nsupprime \n ")
+
+                rescue => e
+                  logger.info("\npost_id"+post["id"]+"\nerreur \n "+e.message)
+                  logger.error("\nil y a eu un probleme pour le post facebook  numero  "+post["id"]+" : "+e.message)
+                  next
+    
+                end
+              end
+          end
+      rescue => e
+        logger.error("\nil y a eu un probleme pour la page de  "+link+" : "+e.message)
+        next
+    
+      end
+    
+    
+end
